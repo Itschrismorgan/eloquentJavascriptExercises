c0ce1a931b2928fa02282d51f9cc72aa3a4498a9
Chris Morgan <chris@itsthemorgans.com>
2014-11-24 08:00:47 -0500

    regex golf

---
+++ chapter9/regex_golf.js
@@ -0,0 +1,47 @@
+/**
+ * Created by chrismorgan on 11/24/14.
+ */
+
+// Fill in the regular expressions
+
+verify(/ca(r|t)/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop"]);
+
+verify(/ferr(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/[a-z]ious\b/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/\s(\.|,|;|:)/,
+    ["bad punctuation ."],
+    ["escape the dot"]);
+
+verify(/\w{7,}/,
+    ["hottentottententen"],
+    ["no", "hotten totten tenten"]);
+
+verify(/\b[a-df-z]+\b/,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "learning ape"]);
+
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return;
+    yes.forEach(function(s) {
+        if (!regexp.test(s))
+            console.log("Failure to match '" + s + "'");
+    });
+    no.forEach(function(s) {
+        if (regexp.test(s))
+            console.log("Unexpected match for '" + s + "'");
+    });
+}
