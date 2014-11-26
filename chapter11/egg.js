cb6fbf466d33d5b5b9966be2518f39aad055d11f
Chris Morgan <chris@itsthemorgans.com>
2014-11-26 11:33:36 -0500

    parsing works

--- chapter11/egg.js
+++ chapter11/egg.js
@@ -2,19 +2,18 @@
  * Created by chrismorgan on 11/25/14.
  */
 
-
-function parseExpression(program){
+function parseExpression(program) {
     program = skipSpace(program);
+    console.log(program);
     var match, expr;
-    if (match = /^"([^"]*)"/.exec(program)){
+    if (match = /^"([^"]*)"/.exec(program))
         expr = {type: "value", value: match[1]};
-    } else if (match = /^\d+\b/.exec(program)){
+    else if (match = /^\d+\b/.exec(program))
         expr = {type: "value", value: Number(match[0])};
-    } else if (match = /^[^\s(),"]=/.exec(program)){
-        expr = {type: "word", value: match[0]};
-    } else {
-        throw new SyntaxError("Unexpected Syntax: "+program);
-    }
+    else if (match = /^[^\s(),"]+/.exec(program))
+        expr = {type: "word", name: match[0]};
+    else
+        throw new SyntaxError("Unexpected syntax: " + program);
 
     return parseApply(expr, program.slice(match[0].length));
 }
@@ -27,24 +26,38 @@ function skipSpace(string){
     return string.slice(first);
 }
 
-function parseApply(expr, program){
+function parseApply(expr, program) {
     program = skipSpace(program);
-    if (program[0] != "("){
+    if (program[0] != "(")
         return {expr: expr, rest: program};
-    }
 
     program = skipSpace(program.slice(1));
-
     expr = {type: "apply", operator: expr, args: []};
-    while (program[0] != "("){
+    while (program[0] != ")") {
         var arg = parseExpression(program);
         expr.args.push(arg.expr);
         program = skipSpace(arg.rest);
-        if(program[0] === ","){
+        if (program[0] == ",")
             program = skipSpace(program.slice(1));
-        } else if (program[0] !== ")") {
+        else if (program[0] != ")")
             throw new SyntaxError("Expected ',' or ')'");
-        }
     }
     return parseApply(expr, program.slice(1));
 }
+
+
+
+function parse(program){
+    var result = parseExpression(program);
+    if(skipSpace(result.rest).length > 0){
+        throw new SyntaxError("Unexpected text after program");
+    }
+    return result.expr;
+}
+
+
+console.log(parse("+(a, 10)"));
+// → {type: "apply",
+//    operator: {type: "word", name: "+"},
+//    args: [{type: "word", name: "a"},
+//           {type: "value", value: 10}]}”
