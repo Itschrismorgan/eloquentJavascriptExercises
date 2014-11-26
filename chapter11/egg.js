60fb5f3563e6fd6f1cfb33fb0af42540e14e326a
Chris Morgan <chris@itsthemorgans.com>
2014-11-26 13:07:08 -0500

    evaluate function

--- chapter11/egg.js
+++ chapter11/egg.js
@@ -4,7 +4,7 @@
 
 function parseExpression(program) {
     program = skipSpace(program);
-    console.log(program);
+    //console.log(program);
     var match, expr;
     if (match = /^"([^"]*)"/.exec(program))
         expr = {type: "value", value: match[1]};
@@ -45,8 +45,6 @@ function parseApply(expr, program) {
     return parseApply(expr, program.slice(1));
 }
 
-
-
 function parse(program){
     var result = parseExpression(program);
     if(skipSpace(result.rest).length > 0){
@@ -54,6 +52,34 @@ function parse(program){
     }
     return result.expr;
 }
+
+function evaluate(expr, env){
+    switch(expr.type){
+        case "value":
+            return expr.value;
+
+        case "word":
+            if(expr.name in env){
+                return env[expr.name];
+            } else {
+                throw new ReferenceError("Undefined variable: "+expr.name);
+            }
+
+        case "apply":
+            if(expr.operator.type === "word" && expr.operator.name in specialForms){
+                return specialForms[expr.operator.name](expr.args, env);
+            }
+            var op = evaluate(expr.operator, env);
+            if (typeof op !== "function"){
+                throw new TypeError("Applying a non-function");
+            }
+            return op.apply(null, expr.args.map(function(arg){
+                return evaluate(arg, env);
+            }));
+    }
+}
+
+var specialForms = Object.create(null);
 
 
 console.log(parse("+(a, 10)"));
