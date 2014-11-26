580c0c6189401c4003d968a0b7968280e717d497
Chris Morgan <chris@itsthemorgans.com>
2014-11-26 11:24:09 -0500

    parseApply function

--- chapter11/egg.js
+++ chapter11/egg.js
@@ -26,3 +26,25 @@ function skipSpace(string){
     }
     return string.slice(first);
 }
+
+function parseApply(expr, program){
+    program = skipSpace(program);
+    if (program[0] != "("){
+        return {expr: expr, rest: program};
+    }
+
+    program = skipSpace(program.slice(1));
+
+    expr = {type: "apply", operator: expr, args: []};
+    while (program[0] != "("){
+        var arg = parseExpression(program);
+        expr.args.push(arg.expr);
+        program = skipSpace(arg.rest);
+        if(program[0] === ","){
+            program = skipSpace(program.slice(1));
+        } else if (program[0] !== ")") {
+            throw new SyntaxError("Expected ',' or ')'");
+        }
+    }
+    return parseApply(expr, program.slice(1));
+}
