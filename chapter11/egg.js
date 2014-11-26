90fba1f251fe7d1fea89f97f26da10183d0ef964
Chris Morgan <chris@itsthemorgans.com>
2014-11-26 13:20:55 -0500

    added egg key words/functions

--- chapter11/egg.js
+++ chapter11/egg.js
@@ -79,8 +79,43 @@ function evaluate(expr, env){
     }
 }
 
-var specialForms = Object.create(null);
+specialForms["if"] = function(args, env){
+    if (args.length !== 3)
+        throw new SyntaxError("Bad number of args to if");
 
+    if(evaluate(args[0],env) !== false)
+        return evaluate(args[1],env);
+    else
+        return evaluate(args[2],env);
+};
+specialForms["while"] = function(args, env){
+    if (args.length !== 2)
+        throw new SyntaxError("Bad number of args to while loop");
+    while(evaluate(args[0], env) !== false){
+        evaluate(args[1],env);
+    }
+
+    return false;
+};
+specialForms["do"] = function(args,env){
+    var value = false;
+    args.forEach(function(arg){
+        value = evaluate(arg,env);
+    });
+    return value;
+};
+specialForms["define"] = function(args,env){
+    if(args.length !== 2 || args[0].type !== "word"){
+        throw new SyntaxError("Bad use of define");
+    }
+    var value = evaluate(args[1], env);
+    env[args[0].name] = value;
+    return value;
+};
+
+
+
+var specialForms = Object.create(null);
 
 console.log(parse("+(a, 10)"));
 // → {type: "apply",
